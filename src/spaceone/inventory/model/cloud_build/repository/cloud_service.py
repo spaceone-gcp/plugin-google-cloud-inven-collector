f4afd548a975495453ede594978e8db7175d40d7
ljieun <ljieun@mz.co.kr>
2025-11-10 20:16:32 +0900

    Bugfix-GCP-INVEN-003-036 > CloudBuild > Repository > Repository Overview : Data Inconsistency

--- src/spaceone/inventory/model/cloud_build/repository/cloud_service.py
+++ src/spaceone/inventory/model/cloud_build/repository/cloud_service.py
@@ -21,7 +21,8 @@ Cloud Build Repository
 repository_overview = ItemDynamicLayout.set_fields(
     "Repository Overview",
     fields=[
-        TextDyField.data_source("Name", "data.full_name"),
+        TextDyField.data_source("Name", "data.name"),
+        TextDyField.data_source("Full Name", "data.full_name"),
         TextDyField.data_source("Connection", "data.connection"),
         TextDyField.data_source("Remote URI", "data.remote_uri"),
         TextDyField.data_source("ETag", "data.etag"),

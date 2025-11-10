5c069dfbe6f77801a95f1c83d9b8c6c3d6567aaa
julia lim <julia.lim@mz.co.kr>
2025-11-10 22:26:11 +0900

    feat: Apply core improvements without sensitive data
    - App Engine monitoring configuration simplification
    - GKE cluster table field changes (CPU/Memory to Machine Type)
    - Unnecessary log and duplicate code cleanup
    - quotaLimits, Fleet/Membership API message improvements
    - Applied core functionality without sensitive test files

--- src/spaceone/inventory/model/kubernetes_engine/cluster/data.py
+++ src/spaceone/inventory/model/kubernetes_engine/cluster/data.py
@@ -305,13 +305,7 @@ class GKECluster(BaseResource):
         deserialize_from="currentNodeCount", serialize_when_none=False
     )
     # Total cluster resources (calculated from node pools)
-    total_cpu = IntType(deserialize_from="total_cpu", serialize_when_none=False)
-    total_memory_gb = StringType(
-        deserialize_from="total_memory_gb", serialize_when_none=False
-    )
-    total_memory_mb = IntType(
-        deserialize_from="total_memory_mb", serialize_when_none=False
-    )
+    machine_type = StringType(deserialize_from="machine_type", serialize_when_none=False)
     create_time = StringType(deserialize_from="createTime", serialize_when_none=False)
     resource_labels = DictType(
         StringType, deserialize_from="resourceLabels", serialize_when_none=False

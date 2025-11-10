54abc9cd5ad2e2b6c834be207161b07ca0f277ef
ljieun <ljieun@mz.co.kr>
2025-11-10 21:47:59 +0900

    Bugfix-GCP-INVEN-003-060 > CloudBuild > WorkerPool > Worker Pool Overview : Name Field Data Inconsistency

--- src/spaceone/inventory/model/cloud_build/worker_pool/cloud_service.py
+++ src/spaceone/inventory/model/cloud_build/worker_pool/cloud_service.py
@@ -22,7 +22,8 @@ worker_pool_overview = ItemDynamicLayout.set_fields(
     "Worker Pool Overview",
     fields=[
         TextDyField.data_source("ID", "data.uid"),
-        TextDyField.data_source("Name", "data.full_name"),
+        TextDyField.data_source("Name", "data.name"),
+        TextDyField.data_source("Full Name", "data.full_name"),
         TextDyField.data_source("State", "data.state"),
         TextDyField.data_source("ETag", "data.etag"),
         DateTimeDyField.data_source("Create Time", "data.create_time"),

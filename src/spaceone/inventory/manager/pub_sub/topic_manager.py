051092d03935ce2bd714a9e50f54d50376a13318
kyeonguk <kyeonguk@mz.co.kr>
2025-11-12 16:30:19 +0900

    Fix: Update Pub/Sub monitoring to use specific metrics

--- src/spaceone/inventory/manager/pub_sub/topic_manager.py
+++ src/spaceone/inventory/manager/pub_sub/topic_manager.py
@@ -116,19 +116,22 @@ class TopicManager(GoogleCloudManager):
                     }
                 )
 
+                # Google Cloud Monitoring 필터 설정
+                google_cloud_monitoring_filters = [
+                    {"key": "resource.labels.topic_id", "value": topic_id},
+                ]
+
                 topic.update(
                     {
                         # Monitoring data
-                        "google_cloud_monitoring": self.set_google_cloud_monitoring(
+                        "google_cloud_monitoring": self._set_multiple_google_cloud_monitoring(
                             project_id,
-                            "pubsub.googleapis.com",
-                            topic_id,
                             [
-                                {
-                                    "key": "resource.labels.topic_id",
-                                    "value": topic_id,
-                                }
+                                "pubsub.googleapis.com/topic/ingestion_byte_count",
+                                "pubsub.googleapis.com/topic/ingestion_message_count",
                             ],
+                            topic_id,
+                            google_cloud_monitoring_filters,
                         ),
                         # Logging data
                         "google_cloud_logging": self.set_google_cloud_logging(
@@ -223,3 +226,29 @@ class TopicManager(GoogleCloudManager):
     def _make_subscription_id(subscription_name):
         *path, subscription_id = subscription_name.split("/")
         return subscription_id
+
+    @staticmethod
+    def _set_multiple_google_cloud_monitoring(
+        project_id, metric_types, resource_id, filters
+    ):
+        """
+        Set multiple Google Cloud Monitoring metric types for PubSub Topic.
+
+        Args:
+            project_id (str): GCP project ID
+            metric_types (list): List of metric types
+            resource_id (str): Resource ID
+            filters (list): Filters to apply to all metric types
+
+        Returns:
+            dict: Google Cloud Monitoring configuration with multiple metric types
+        """
+        monitoring_filters = []
+        for metric_type in metric_types:
+            monitoring_filters.append({"metric_type": metric_type, "labels": filters})
+
+        return {
+            "name": f"projects/{project_id}",
+            "resource_id": resource_id,
+            "filters": monitoring_filters,
+        }

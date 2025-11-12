7d06feeb7299730cab4cbcd592a03443a3ab424f
LimChaeyeon <cylim@mz.co.kr>
2025-11-12 15:25:30 +0900

    Bugfix-GCP-INVEN-018-029 > Networking > VPCGateway > details 탭 > 하위 탭 미노출

--- src/spaceone/inventory/model/networking/vpc_gateway/cloud_service.py
+++ src/spaceone/inventory/model/networking/vpc_gateway/cloud_service.py
@@ -5,6 +5,9 @@ from spaceone.inventory.libs.schema.cloud_service import (
     CloudServiceResource,
     CloudServiceResponse,
 )
+from spaceone.inventory.model.networking.vpc_gateway.cloud_service_type import (
+    vpc_gateway_cloud_service_meta,
+)
 from spaceone.inventory.model.networking.vpc_gateway.data import VPCGateway
 
 """
@@ -16,7 +19,11 @@ class VPCGatewayResource(CloudServiceResource):
     cloud_service_group = StringType(default="Networking")
     cloud_service_type = StringType(default="VPCGateway")
     data = ModelType(VPCGateway)
-    _metadata = ModelType(CloudServiceMeta, serialize_when_none=False)
+    _metadata = ModelType(
+        CloudServiceMeta,
+        default=vpc_gateway_cloud_service_meta,
+        serialized_name="metadata",
+    )
 
 
 class VPCGatewayResponse(CloudServiceResponse):

73f556dbac6adf2d4c752c5b74d037b105e82cf7
Minhyeok LEE <130422754+raccoon-mh@users.noreply.github.com>
2025-11-17 18:12:03 +0900

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         "grpcio",
         "spaceone-api",
-        "spaceone-core==1.12.37",
+        "spaceone-core",
         "schematics",
         "google-api-python-client",
         "MarkupSafe>=2.0.0rc2",
